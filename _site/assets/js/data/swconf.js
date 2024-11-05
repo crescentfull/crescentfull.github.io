a7db5f9681e5c2ff44c466d7cf6266c2e3538b20
Yeongrok Song <song7868@gmail.com>
2024-11-05 17:28:32 +0900

    test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730718261',resources: [
+    cacheName: 'chirpy-1730795295',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

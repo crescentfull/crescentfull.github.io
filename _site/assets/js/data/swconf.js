20c0a17cbdeced8787a8c9c621fca992431e533b
Yeongrok Song <song7868@gmail.com>
2024-11-14 15:27:20 +0900

    fix: heap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731563462',resources: [
+    cacheName: 'chirpy-1731565630',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

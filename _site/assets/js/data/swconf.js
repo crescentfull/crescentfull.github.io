2756224dd0acc155dfd88d644db6d115bfc53551
Yeongrok Song <song7868@gmail.com>
2024-11-14 15:26:55 +0900

    feat: heap

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731563262',resources: [
+    cacheName: 'chirpy-1731563462',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

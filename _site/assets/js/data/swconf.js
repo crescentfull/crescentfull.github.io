e16e7293ef2466dfd46797024565e281d8202c45
Yeongrok Song <song7868@gmail.com>
2024-11-18 18:06:13 +0900

    feat: 모두의 네트워크 5장

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731920572',resources: [
+    cacheName: 'chirpy-1731920753',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

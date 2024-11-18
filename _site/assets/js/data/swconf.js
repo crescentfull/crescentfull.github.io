5776524077fc1993d7b15a45e479d44a7ebfa655
Yeongrok Song <song7868@gmail.com>
2024-11-18 19:53:33 +0900

    fix: http status

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731925501',resources: [
+    cacheName: 'chirpy-1731927173',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

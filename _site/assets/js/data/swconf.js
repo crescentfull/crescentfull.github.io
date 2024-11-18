02ac99bc31fdb8045f52880ed7699602de353346
Yeongrok Song <song7868@gmail.com>
2024-11-18 19:57:23 +0900

    feat: class

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731927173',resources: [
+    cacheName: 'chirpy-1731927432',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

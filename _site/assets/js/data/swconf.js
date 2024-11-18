2364bcd13aef1f38623a68170b2b38f5bdbc54a1
Yeongrok Song <song7868@gmail.com>
2024-11-18 21:55:40 +0900

    feat: MultiThreading

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731930982',resources: [
+    cacheName: 'chirpy-1731934458',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
